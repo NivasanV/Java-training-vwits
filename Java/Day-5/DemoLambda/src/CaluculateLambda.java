8e593e8ee7ed305f85d8babc9670ae934003394e
Nivasan <58383171+NivasanV@users.noreply.github.com>
2023-10-26 04:08:34 +0000

    done

--- Java/Day-5/DemoLambda/src/CaluculateLambda.java
+++
@@ -1,5 +0,0 @@
-public class CaluculateLambda {
-    public static void main(String[] args) {
-        
-    }
-}
--- Java/Day-5/DemoLambda/src/Taxable.java
+++ Java/Day-5/DemoLambda/src/Taxable.java
@@ -1,3 +1,3 @@
-interface Taxable{
+public interface Taxable{
    	double calculateTax(double income);
 }
